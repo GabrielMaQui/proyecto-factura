88c3af02e47a1c8146a6594964e6fed141eec9e8
Gabriel <gmachicaoq@unsa.edu.pe>
2024-05-09 16:59:53 -0500

    Agregando el CSRF a nuestro código, y culimando el eliminar del proceso

--- src/main/resources/static/js/web/backoffice/product/formproduct.js
+++ src/main/resources/static/js/web/backoffice/product/formproduct.js
@@ -1,3 +1,8 @@
+let token = $("meta[name='_csrf']").attr("content");
+let header = $("meta[name='_csrf_header']").attr("content");
+$(document).ajaxSend(function(e, xhr, options) {
+    xhr.setRequestHeader(header, token);
+});
 $(document).on("click", "#btnagregar",function () {
     $("#txtnomproduct").val(" ");
     $("#txtunitpriceproduct").val(" ")
@@ -24,6 +29,9 @@ $(document).on("click", ".btnactualizar", function () {
 
     $("#modalproduct").modal("show");
 });
+
+
+
 
 $(document).on("click", "#btnguardar", function () {
     let create = convertirFechaStringADate($("#txtfechacreacion").val());
@@ -53,6 +61,29 @@ $(document).on("click", "#btnguardar", function () {
     $("#modalproduct").modal("hide");
 });
 
+
+$(document).on("click", ".btneliminar", function () {
+
+    let productId = $(this).data("prodcod");
+
+    if (confirm("¿Estás seguro de que deseas eliminar este producto?")) {
+        // Enviar solicitud AJAX para eliminar el producto
+        $.ajax({
+            type: "POST",
+            url: "/producto/delete/" + productId,
+
+            success: function (resultado) {
+                console.log(resultado);
+                listarProducto();
+            },
+            error: function (xhr, status, error) {
+                console.error("Error en la solicitud AJAX:", error);
+                alert("Error en la solicitud AJAX. Por favor, inténtalo de nuevo más tarde.");
+            }
+        });
+    }
+});
+
 function listarProducto() {
     $.ajax({
         type: "GET",
@@ -73,31 +104,16 @@ function listarProducto() {
                     `data-produnit="${value.precio}" ` +
                     `data-prodcreat="${value.createAt}" ` +
                     `data-prodstock="${value.stock}" ` +
-                    `data-proddiscont="${value.discontinued}">Actualizar` +
+                    `data-proddiscont="${value.activo}">Actualizar` +
+                    `</button><td><button type="button" class="btn btn-danger btneliminar"` +
+                    `data-prodcod="${value.id}">Eliminar` +
+                    `</button></td>`+
                     `</button></td>` +
                     `</tr>`);
             });
         }
     });
 }
-// $(document).on("click", ".btneliminar", function () {
-//     if (confirm("¿Estás seguro de que quieres eliminar este producto?")) {
-//         let productoId = $(this).data("prodid");
-//
-//         $.ajax({
-//             type: "POST",
-//             url: "/producto/eliminar/" + productoId,
-//             success: function (resultado) {
-//                 listarProducto();
-//                 alert("Producto eliminado correctamente");
-//             },
-//             error: function (xhr, status, error) {
-//                 console.error("Error al eliminar el producto:", error);
-//                 alert("Error al eliminar el producto. Por favor, inténtalo de nuevo más tarde.");
-//             }
-//         });
-//     }
-// });
 
 function convertirFechaStringADate(fechaString) {
     let partesFecha = fechaString.split("-");
